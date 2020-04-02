3fd1ada969c65199e4e6d2fdc9ab2179b5742dc3
Nida Acar <46358150+nidaacar@users.noreply.github.com>
2020-04-02 19:03:12 +0300

    Add files via upload

---
+++ AlternatingCharacters.cs
@@ -0,0 +1,36 @@
+﻿
+class Solution
+{
+    static int alternatingCharacters(string s)
+    {
+        char[] arr = s.ToCharArray();
+        int count = 0;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] == arr[i + 1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = alternatingCharacters(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
